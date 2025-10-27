2b96906671519854f32d8add9519936f87ec0c54
elt7613 <elt7613@gmail.com>
2025-10-27 20:25:58 +0530

    first commit

---
+++ dockploy.py
@@ -0,0 +1,28 @@
+
+import requests
+
+API_URL = "http://18.118.247.36:3000/api/project.all"
+api_key = "xTDMcSEvOHvFyomBUOKmWjodbQPwtEIcckByXTHCooltdQmHSMFFkEYjxkwFraaW"
+app_name = "job-portal-backend-nuf1sg"
+
+
+def get_application_id():
+   """Fetch Application ID"""
+   headers = {
+       "accept": "application/json",
+       "x-api-key": api_key,
+   }
+   req = requests.get(API_URL, headers=headers, timeout=30)
+   req.raise_for_status()
+   data = req.json()
+   # Safely iterate and find the requested application by name
+   for project in data or []:
+       environments = project.get("environments", [])
+       for env in environments:
+           applications = env.get("applications", [])
+           for app in applications:
+               if app.get("appName") == app_name:
+                   print(app.get("applicationId"))
+   return req.content
+
+get_application_id()
